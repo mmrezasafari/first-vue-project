6398e1f00fecf57a323cc8f88dc43f1cf3e7d754
mmreza <mmrezadev@gmail.com>
2022-06-18 11:49:03 +0430

    complete auto input page

--- src/views/AutoInput.vue
+++ src/views/AutoInput.vue
@@ -1,30 +1,27 @@
 <script>
-    export default{
-        data() {
-            return{
-                valueLenght: null
-            }
-        },
-        methods: {
-            onChange(e, n) {
-                if(e.target.value > 4){
-                    this.$refs.input-n.focus()
+export default {
+    methods: {
+        onChange(e) {
+            if ((e.target.value).toString().length > 3) {
+                if (e.target.nextElementSibling) {
+                    e.target.nextElementSibling.focus();
                 }
             }
         }
     }
+}
 </script>
 
 <template>
     <article class="container">
         <div class="row h-100 d-flex  align-items-center justify-content-center">
-            <section class=" inputs col-12 d-flex justify-content-center align-items-center">
+            <section class=" inputs col-12 d-flex flex-column flex-md-row justify-content-center align-items-center">
                 <input
-                    v-for="n in 4"
-                    @keyup="onChange($event, n)"
-                    type="text"
-                    class="auto-input m-1 px-3 py-1"
-                    :ref="`input-${n}`"
+                    v-for="n in 4" 
+                    @keyup="onChange($event)" 
+                    type="text" 
+                    class="input m-1 px-3 py-1 col-3 col-md-12"
+                    maxlength="4"
                     :key="n"
                 >
             </section>
@@ -32,17 +29,16 @@
     </article>
 </template>
 <style>
-
-.inputs{
+.inputs {
     width: 50%;
-    height: 5rem;
     background-color: white;
     border-radius: 5px;
+    padding: 10px 0 ;
 }
-.auto-input{
+
+.input {
     width: 5rem;
     height: 2rem;
     background-color: rgba(128, 128, 128, 0.288);
 }
-
 </style>
